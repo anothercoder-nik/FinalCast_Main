c6c640fb40e38a988b8f1dc2c3033f34497653e2
Nikunj Agarwal <68611481+anothercoder-nik@users.noreply.github.com>
2025-08-06 00:56:25 +0530

    Update config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,7 +1,7 @@
-res.cookie("accessToken", token, {
+export const cookieOptions = {
     httpOnly: true,
     secure: true,
     domain: ".finalcast.tech",   // IMPORTANT: subdomain-aware cookie
     sameSite: 'None',            // Required for cross-site requests
-    maxAge: 7 * 24 * 60 * 60 * 1000  // 7 days
-});
+    maxAge: 7 * 24 * 60 * 60 * 1000 
+}
